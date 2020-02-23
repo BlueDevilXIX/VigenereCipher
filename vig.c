f6fe77c4de9df3e8eac6f2638bad1446cd259d0d
Patrick T. McKee <bludevil@vt.edu>
2020-02-22 22:12:38 -0500

    part 2: Mg table and keyword

--- vig.c
+++ vig.c
@@ -700,7 +700,7 @@ int main(){
 
 
 			//generate Mg table
-                        printf("%10d %10d %10d %10d %10d %10d %10d %10d", 0, 1, 2, 3, 4, 5, 6, 7);
+                        printf("%10d %10d %10d %10d %10d %10d %10d \n", 1, 2, 3, 4, 5, 6, 7);
                         float f[26];
                         for (i = 0; i < 26; ++i)
                                 f[i] = 0;
@@ -711,10 +711,25 @@ int main(){
                                                 f[j]++;
                                 }
                         }
+			//frequency table
+			float p[] = {.082, .015, .028, .043, .127, .022, .020, .061, .070, .002, .008, .040, .024, .067, .075, .019, .001, .060, .063, .091, .028, .010, .023, .001, .020, .001};
+			float q[26];
+			
+			
                         for (i = 0; i < 26; ++i){
-                                ioc1 += f[i]*(f[i]-1);
+                                q[i] = f[i] / size;
                         }
-
+			float v[26];
+			float mg1[26];
+			int g;
+			for (g = 0; g < 26; ++g){
+				for (i = 0; i < 26; ++i){
+					v[i] = q[(i+g)%26];
+					mg1[g] += v[i] * p[i];
+				}
+			}
+			
+			//repeat for y2 through y7
                         for (i = 0; i < 26; ++i)
                                 f[i] = 0;
                         for (i = 0; i < size; ++i){
@@ -724,14 +739,21 @@ int main(){
                                                 f[j]++;
                                 }
                         }
-                        float ioc2 = 0;
+                        float q2[26];
                         for (i = 0; i < 26; ++i){
-                                ioc2 += f[i]*(f[i]-1);
+                                q2[i] = f[i] / size;
                         }
-                        ioc2 /= size*(size-1);
-                        printf("Ic(y2) = %f \n", ioc2);
+                        float v2[26];
+                        float mg2[26];
+                        for (g = 0; g < 26; ++g){
+                                for (i = 0; i < 26; ++i){
+                                        v2[i] = q2[(i+g)%26];
+                                        mg2[g] += v2[i] * p[i];
+                                }
+                        }
+			
 
-
+			
                         for (i = 0; i < 26; ++i)
                                 f[i] = 0;
                         for (i = 0; i < size; ++i){
@@ -741,12 +763,21 @@ int main(){
                                                 f[j]++;
                                 }
                         }
-                        float ioc3 = 0;
+                        float q3[26];
                         for (i = 0; i < 26; ++i){
-                                ioc3 += f[i]*(f[i]-1);
+                                q3[i] = f[i] / size;
                         }
-                        ioc3 /= size*(size-1);
-                        printf("Ic(y3) = %f \n", ioc3);
+                        float v3[26];
+                        float mg3[26];
+                        for (g = 0; g < 26; ++g){
+                                for (i = 0; i < 26; ++i){
+                                        v3[i] = q3[(i+g)%26];
+                                        mg3[g] += v3[i] * p[i];
+                                }
+                        }
+
+
+
 
 
 
@@ -759,12 +790,20 @@ int main(){
                                                 f[j]++;
                                 }
                         }
-                        float ioc4 = 0;
+                        float q4[26];
                         for (i = 0; i < 26; ++i){
-                                ioc4 += f[i]*(f[i]-1);
+                                q4[i] = f[i] / size;
                         }
-                        ioc4 /= size*(size-1);
-                        printf("Ic(y4) = %f \n", ioc4);
+                        float v4[26];
+                        float mg4[26];
+                        for (g = 0; g < 26; ++g){
+                                for (i = 0; i < 26; ++i){
+                                        v4[i] = q4[(i+g)%26];
+                                        mg4[g] += v4[i] * p[i];
+                                }
+                        }
+
+
 
 
                         for (i = 0; i < 26; ++i)
@@ -776,12 +815,20 @@ int main(){
                                                 f[j]++;
                                 }
                         }
-                        float ioc5 = 0;
+                        float q5[26];
                         for (i = 0; i < 26; ++i){
-                                ioc5 += f[i]*(f[i]-1);
+                                q5[i] = f[i] / size;
                         }
-                        ioc5 /= size*(size-1);
-                        printf("Ic(y5) = %f \n", ioc5);
+                        float v5[26];
+                        float mg5[26];
+                        for (g = 0; g < 26; ++g){
+                                for (i = 0; i < 26; ++i){
+                                        v5[i] = q5[(i+g)%26];
+                                        mg5[g] += v5[i] * p[i];
+                                }
+                        }
+
+
 
 
 
@@ -794,12 +841,20 @@ int main(){
                                                 f[j]++;
                                 }
                         }
-                        float ioc6 = 0;
+                        float q6[26];
                         for (i = 0; i < 26; ++i){
-                                ioc6 += f[i]*(f[i]-1);
+                                q6[i] = f[i] / size;
                         }
-                        ioc6 /= size*(size-1);
-                        printf("Ic(y6) = %f \n", ioc6);
+                        float v6[26];
+                        float mg6[26];
+                        for (g = 0; g < 26; ++g){
+                                for (i = 0; i < 26; ++i){
+                                        v6[i] = q6[(i+g)%26];
+                                        mg6[g] += v6[i] * p[i];
+                                }
+                        }
+
+
 
 
 
@@ -813,15 +868,28 @@ int main(){
                                                 f[j]++;
                                 }
                         }
-                        float ioc7 = 0;
+                        float q7[26];
                         for (i = 0; i < 26; ++i){
-                                ioc7 += f[i]*(f[i]-1);
+                                q7[i] = f[i] / size;
                         }
-                        ioc7 /= size*(size-1);
-                        printf("Ic(y7) = %f \n", ioc7);
+                        float v7[26];
+                        float mg7[26];
+                        for (g = 0; g < 26; ++g){
+                                for (i = 0; i < 26; ++i){
+                                        v7[i] = q7[(i+g)%26];
+					if (g == 20)
+						printf("%f \n", v7[i]);
+                                        mg7[g] += v7[i] * p[i];
+                                }
+                        }
 
-
+			//print the table
+			char d[] = "Mg";
+			printf("g %10s %10s %10s %10s %10s %10s %10s \n", d, d, d, d, d, d, d);
+			for (g = 0; g < 26; ++g)
+				printf("%.2d %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f \n", g, mg1[g], mg2[g], mg3[g], mg4[g], mg5[g], mg6[g], mg7[g]);
 
 
 	return 0;
-}	
+
+}
